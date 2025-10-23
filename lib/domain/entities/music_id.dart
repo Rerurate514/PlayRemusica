5d253f61b4f7ff00196150fd5328845a9d78c5cb
Rerurate514 <rerurateyuto@gmail.com>
2025-10-23 12:07:48 +0900

    idオブジェクトを再定義

--- lib/domain/entities/music_id.dart
+++ lib/domain/entities/music_id.dart
@@ -1,13 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:playremusica/domain/entities/picture_image.dart';
 
 part 'music_id.freezed.dart';
 
 @freezed
 sealed class MusicId with _$MusicId {
   const factory MusicId({
-    @Default(100) int volume,
-    required String lyrics,
-    required PictureImage picture,
+    required String id
   }) = _MusicId;
 }
--- lib/domain/entities/playlist_id.dart
+++ lib/domain/entities/playlist_id.dart
@@ -1,13 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:playremusica/domain/entities/picture_image.dart';
 
 part 'playlist_id.freezed.dart';
 
 @freezed
 sealed class PlayListId with _$PlayListId {
   const factory PlayListId({
-    @Default(100) int volume,
-    required String lyrics,
-    required PictureImage picture,
+    required String id
   }) = _PlayListId;
 }
