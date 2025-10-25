f8152a2bd613a22d2e1bc3a731e2125d24f80a82
Rerurate514 <rerurateyuto@gmail.com>
2025-10-25 20:33:12 +0900

    interfaceを分離

---
+++ lib/domain/repositories/music_db_reposiyory_interface.dart
@@ -0,0 +1,10 @@
+import 'package:playremusica/domain/entities/music.dart';
+import 'package:playremusica/domain/entities/music_id.dart';
+import 'package:playremusica/domain/entities/music_settings.dart';
+
+abstract class IMusicDbRepository {
+  Future<void> saveMusic(Music music);
+  Future<Music> readMusicById(MusicId id);
+  Future<void> deleteMusic(MusicId id);
+  Future<void> updateMusicSettings(MusicId id, MusicSettings settings);
+}
---
+++ lib/domain/repositories/playlist_db_repository_interface.dart
@@ -0,0 +1,9 @@
+import 'package:playremusica/domain/entities/playlist.dart';
+import 'package:playremusica/domain/entities/playlist_id.dart';
+
+abstract class IPlayListDbRepository {
+  Future<void> savePlayList(PlayList playlist);
+  Future<List<PlayList>> readAllPlayLists();
+  Future<void> deletePlayList(PlayListId id);
+  Future<void> updatePlayList(PlayListId id);
+}
