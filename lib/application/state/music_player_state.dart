ac1087ab1ff6e78bc927d94467dbfe944d1eec2e
Rerurate514 <rerurateyuto@gmail.com>
2025-10-25 17:39:02 +0900

    MusicPlayerStateの作成

---
+++ lib/application/state/music_player_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:playremusica/domain/services/player_domain_service.dart';
+
+part 'music_player_state.freezed.dart';
+
+@freezed
+sealed class MusicPlayerState with _$MusicPlayerState {
+  const factory MusicPlayerState({
+    required bool isPlaying,
+    required PlayerDomainService pds
+  }) = _MusicPlayerState;
+}
