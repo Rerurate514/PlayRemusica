ce145fe2bffdab82178f3783880843f14f049316
Rerurate514 <rerurateyuto@gmail.com>
2025-10-28 16:43:45 +0900

    playlistのpictureをnullableに変更

--- lib/infrastructure/models/playlist_table.dart
+++ lib/infrastructure/models/playlist_table.dart
@@ -3,5 +3,5 @@ import 'package:drift/drift.dart';
 class PlayListTable extends Table {
   TextColumn get id => text().unique()();
   TextColumn get name => text()();
-  BlobColumn get picture => blob()();
+  BlobColumn get picture => blob().nullable()();
 }
