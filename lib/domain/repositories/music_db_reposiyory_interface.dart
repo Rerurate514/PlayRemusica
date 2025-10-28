747ba46f494efd225ed90d2b48cea59dbb9fd347
Rerurate514 <rerurateyuto@gmail.com>
2025-10-28 16:45:59 +0900

    リポジトリインターフェースの定義メソッドを追加

--- lib/domain/repositories/music_db_reposiyory_interface.dart
+++ lib/domain/repositories/music_db_reposiyory_interface.dart
@@ -5,6 +5,7 @@ import 'package:playremusica/domain/entities/music_settings.dart';
 abstract class IMusicDbRepository {
   Future<void> saveMusic(Music music);
   Future<Music> readMusicById(MusicId id);
+  Future<List<Music>> readAllMusics();
   Future<void> deleteMusic(MusicId id);
   Future<void> updateMusicSettings(MusicId id, MusicSettings settings);
 }
