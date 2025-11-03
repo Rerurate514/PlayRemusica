738535be1299457b962313561de86358ef90ff1f
Rerurate514 <rerurateyuto@gmail.com>
2025-11-03 09:35:03 +0900

    playlist関係のrepositoryの作成

--- lib/domain/repositories/playlist_db_repository_interface.dart
+++ lib/domain/repositories/playlist_db_repository_interface.dart
@@ -1,11 +1,9 @@
-import 'package:playremusica/domain/entities/music.dart';
 import 'package:playremusica/domain/entities/playlist.dart';
 import 'package:playremusica/domain/entities/playlist_id.dart';
 
 abstract class IPlayListDbRepository {
   Future<void> savePlayList(PlayList playlist);
-  Future<PlayList> readMusicById(PlayListId id);
   Future<List<PlayList>> readAllPlayLists();
   Future<void> deletePlayList(PlayListId id);
-  Future<void> updateList(PlayListId id, List<Music> musics);
+  Future<void> updatePlayList(PlayListId id, PlayList playlist);
 }
--- lib/infrastructure/repositories/playlist_db_repository.dart
+++ lib/infrastructure/repositories/playlist_db_repository.dart
@@ -1,6 +1,8 @@
-import 'dart:typed_data';
-
 import 'package:drift/drift.dart';
+import 'package:playremusica/domain/entities/music.dart';
+import 'package:playremusica/domain/entities/music_id.dart';
+import 'package:playremusica/domain/entities/music_settings.dart';
+import 'package:playremusica/domain/entities/picture_image.dart';
 import 'package:playremusica/domain/entities/playlist.dart';
 import 'package:playremusica/domain/entities/playlist_id.dart';
 import 'package:playremusica/domain/repositories/playlist_db_repository_interface.dart';
@@ -37,11 +39,6 @@ class PlayListDbRepositoryImpl implements IPlayListDbRepository {
   }
 
   @override
-  Future<List<PlayList>> readAllPlayLists() {
-    
-  }
-
-  @override
   Future<void> savePlayList(PlayList playlist) async {
     final image = playlist.picture.imageProvider;
 
@@ -58,8 +55,83 @@ class PlayListDbRepositoryImpl implements IPlayListDbRepository {
   }
 
   @override
-  Future<void> updatePlayList(PlayListId id) {
-    // TODO: implement updatePlayList
-    throw UnimplementedError();
+  Future<List<PlayList>> readAllPlayLists() async {
+    final Map<String, List<MusicTableData>> playlistMusicMap = {};
+
+    final query = db.select(db.playListTable).join([
+      innerJoin(
+        db.playlistItemsTable, 
+        db.playlistItemsTable.playlistId.equalsExp(db.playListTable.id)
+      ),
+      innerJoin(
+        db.musicTable, 
+        db.musicTable.id.equalsExp(db.playlistItemsTable.musicId))
+    ]);
+
+    final result = await query.get();
+
+    for (final row in result) {
+      final playlistData = row.readTable(db.playListTable);
+      final musicData = row.readTable(db.musicTable);
+
+      final playlistId = playlistData.id;
+
+      if (!playlistMusicMap.containsKey(playlistId)) {
+        playlistMusicMap[playlistId] = [];
+      }
+      playlistMusicMap[playlistId]!.add(musicData);
+    }
+
+    final allPlayListBasicData = await db.select(db.playListTable).get();
+
+    final List<PlayList> playlists = allPlayListBasicData.map((PlayListTableData data) {
+      final String playlistId = data.id;
+      
+      final List<MusicTableData> musicDataList = playlistMusicMap[playlistId] ?? [];
+      
+      final List<Music> musicList = musicDataList.map((data) {
+        return Music(
+          id: MusicId(id: data.id), 
+          name: data.name, 
+          path: data.filePath, 
+          musicSettings: MusicSettings(
+            lyrics: data.lyrics, 
+            picture: PictureImage(
+              imageProvider: data.picture != null ? pbc.convertBytesToImage(data.picture!) : null
+            )
+          )
+        );
+      }).toList();
+      
+      return PlayList(
+        id: PlayListId(id: data.id),
+        name: data.name,
+        list: musicList,
+        picture: PictureImage(
+          imageProvider: data.picture != null ? pbc.convertBytesToImage(data.picture!) : null
+        )
+      );
+    }).toList();
+
+    return playlists;
+  }
+
+  @override
+  Future<void> updatePlayList(PlayListId id, PlayList playlist) async {
+    final image = playlist.picture.imageProvider;
+
+    final Uint8List? pictureBytes = image != null 
+    ? await pbc.convertImageToBytes(image)
+    : null;
+
+    final query = db.update(db.playListTable)
+      ..where((tbl) => tbl.id.equals(id.id));
+
+    await query.write(
+      PlayListTableCompanion(
+        name: Value(playlist.name),
+        picture: Value(pictureBytes),
+      )
+    );
   }
 }
