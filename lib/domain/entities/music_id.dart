d76551a191d3f477b7216c9275d18fb170410840
Rerurate514 <rerurateyuto@gmail.com>
2025-11-03 11:58:47 +0900

    idファクトリを作成

--- lib/domain/entities/music_id.dart
+++ lib/domain/entities/music_id.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
 
 part 'music_id.freezed.dart';
 
@@ -7,4 +8,10 @@ sealed class MusicId with _$MusicId {
   const factory MusicId({
     required String id
   }) = _MusicId;
+
+  factory MusicId.fromInputName(String inputName) {
+    const uuid = Uuid();
+    final uniqueId = uuid.v5(Namespace.url.value, inputName);
+    return MusicId(id: uniqueId);
+  }
 }
--- lib/domain/entities/playlist_id.dart
+++ lib/domain/entities/playlist_id.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
 
 part 'playlist_id.freezed.dart';
 
@@ -7,4 +8,10 @@ sealed class PlayListId with _$PlayListId {
   const factory PlayListId({
     required String id
   }) = _PlayListId;
+
+    factory PlayListId.fromInputName(String inputName) {
+    const uuid = Uuid();
+    final uniqueId = uuid.v5(Namespace.url.value, inputName);
+    return PlayListId(id: uniqueId);
+  }
 }
