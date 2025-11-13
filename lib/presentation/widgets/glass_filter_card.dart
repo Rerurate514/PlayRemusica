d1b4ab758c9615f0d512a075375514855fc4b766
Rerurate514 <rerurateyuto@gmail.com>
2025-11-13 12:17:00 +0900

    ガラスカードの作成

---
+++ lib/presentation/widgets/glass_filter_card.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GlassFilterCard extends StatelessWidget {
+  final Widget child;
+  
+  const GlassFilterCard({
+    super.key,
+    required this.child
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: Colors.transparent,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white.withOpacity(0.1),// ignore: deprecated_member_use
+                  Colors.white.withOpacity(0.01),// ignore: deprecated_member_use
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Center(
+              child: child
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
