5a7871d4dca488cdaac19ec2756cb0ae83afffd8
Rerurate514 <rerurateyuto@gmail.com>
2025-11-16 00:36:40 +0900

    routerObserverの削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:playremusica/core/settings/app_theme_data.dart';
-import 'package:playremusica/core/utils/router_observer.dart';
 import 'package:playremusica/features/splash/presentation/pages/splash_page.dart';
 
 void main() {
@@ -14,7 +13,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorObservers: [routeObserver],
       theme: appThemeData,
       home: SplashPage()
     );
