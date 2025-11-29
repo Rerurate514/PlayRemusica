57437d428518639392bd9d41188074af91d03040
Rerurate514 <rerurateyuto@gmail.com>
2025-11-29 23:44:42 +0900

    radiusに名称変更

--- lib/presentation/widgets/ink_card.dart
+++ lib/presentation/widgets/ink_card.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 class InkCard extends StatelessWidget{
   final Function() onTap;
   final Widget child;
-  final double circular;
+  final double radius;
   final double padding;
 
   const InkCard({
     super.key, 
     required this.onTap, 
     required this.child,
-    this.circular = 16.0,
+    this.radius = 16.0,
     this.padding = 0.0,
   });
 
@@ -18,10 +18,10 @@ class InkCard extends StatelessWidget{
   Widget build(BuildContext context){
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(circular)
+        borderRadius: BorderRadius.circular(radius)
       ),
       child: InkWell(
-        borderRadius: BorderRadius.circular(circular),
+        borderRadius: BorderRadius.circular(radius),
         onTap: onTap,
         child: Padding(
           padding: EdgeInsets.all(padding),
