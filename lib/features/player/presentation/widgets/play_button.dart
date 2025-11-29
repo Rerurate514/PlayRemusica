703bb38bfa6df22285b4a54f43facb79c0ab2478
Rerurate514 <rerurateyuto@gmail.com>
2025-11-30 00:27:05 +0900

    playButtonを追加

---
+++ lib/features/player/presentation/widgets/play_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:playremusica/features/player/presentation/viewmodel/play_page_view_model.dart';
+import 'package:playremusica/presentation/widgets/glass_filter_card.dart';
+import 'package:playremusica/presentation/widgets/ink_card.dart';
+
+class PlayButton extends HookConsumerWidget {
+  const PlayButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(playPageViewModelProvider);
+    final prov = ref.watch(playPageViewModelProvider.notifier);
+    final isPlaying = ref.watch(playPageViewModelProvider.select(
+      (state) => state.value?.isPlaying ?? false,
+    ));
+    
+    return InkCard(
+      radius: 256,
+      onTap: () {
+        if(isPlaying) {
+          prov.pause();
+        } else {
+          prov.resume();
+        }
+      }, 
+      child: GlassFilterCard(
+        radius: 256,
+        child: Padding(
+          padding: EdgeInsets.all(14),
+          child: Icon(
+            isPlaying
+            ? Icons.pause
+            : Icons.play_arrow,
+            size: 45,
+          ),
+        ),
+      ),
+    );
+  }
+}
